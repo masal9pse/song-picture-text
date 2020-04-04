be183bb16a247b793e8a4f88d81b6618d12ef8b0
masato <returnymgstokh@icloud.com>
2020-04-04 18:02:12 +0900

    テスト

--- tests/Feature/SongTest.php
+++ tests/Feature/SongTest.php
@@ -16,16 +16,21 @@ class SongTest extends TestCase
   */
  public function testSong()
  {
-  // $this->visit('/') //  掲示板のトップページにアクセスしてみる
-  //  ->see('Laravel') //           「掲示板」という文字列が見える
-  //  ->see('愛を込めて花束を') //         「新規投稿」という文字列もある
-  //  ->click('愛を込めて花束を') //        新規投稿リンクをクリックしてみる
-  //  ->seePageIs('/songs/2') // 新規投稿ページに遷移する
-  //  ->see('いつまでもそばにいて'); //     新規投稿ページには「新規記事投稿」という文字列がある
-  // // $this->assertTrue(true);
-  $this->browse(function ($browser) {
-   $browser->visit('/')
-    ->assertSee('愛を込めて');
-  });
+  $response = $this->get('/');
+
+  $response->assertStatus(200);
+ }
+
+ public function testBasicSong()
+ {
+  $response = $this->withHeaders([
+   'X-Header' => 'Value',
+  ])->json('POST', '/user', ['name' => 'Sally']);
+
+  $response
+   ->assertStatus(200)
+   ->assertJson([
+    'created' => true
+   ]);
  }
 }
