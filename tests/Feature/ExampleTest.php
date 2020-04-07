a88efa9354ec7d5ae266f529e42db9b4e6f928af
masato <returnymgstokh@icloud.com>
2020-04-08 08:10:32 +0900

    vendorの変化じゃないっぽい

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -19,4 +19,25 @@ class ExampleTest extends TestCase
 
   $response->assertStatus(200);
  }
+
+ public function testBasicExample()
+ {
+  $this->withoutExceptionHandling();
+
+  $data = [
+   'name' => 'testUser',
+   'email' => 'dummy@email.com',
+   'password' => 'test1234',
+   'password_confirmation' => 'test1234'
+  ];
+  $response = $this->withHeaders([
+   // 'X-Header' => 'Value',
+  ])->json('POST', route('register'), $data);
+
+  $response
+   ->assertStatus(302);
+  // ->assertJson([
+  //  'created' => true,
+  // ]);
+ }
 }
