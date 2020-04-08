363af7215ee07e4adb96e50590d7862bbcead211
masato <returnymgstokh@icloud.com>
2020-04-08 18:54:19 +0900

    songテーブルにカラムがあるかチェック

--- tests/Feature/Database/DatabaseSongTest.php
+++ tests/Feature/Database/DatabaseSongTest.php
@@ -5,16 +5,23 @@ namespace Tests\Feature\Database;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSongTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+ use RefreshDatabase;
+ /**
+  * A basic test example.
+  *
+  * @return void
+  */
+ public function testSongDatabase()
+ {
+  $this->assertTrue(
+   Schema::hasColumns('books', [
+    'id', 'title', 'author'
+   ]),
+   // 1
+  );
+ }
 }
