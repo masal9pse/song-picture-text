cba3624d3f5f47d020af7b601c2f9e4c4d7307dc
masato <returnymgstokh@icloud.com>
2020-04-09 11:41:50 +0900

    songsテーブルのデータをテスト

--- tests/Feature/Database/DatabaseSongTest.php
+++ tests/Feature/Database/DatabaseSongTest.php
@@ -6,16 +6,17 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Song;
 
 class DatabaseSongTest extends TestCase
 {
- use RefreshDatabase;
+ // use RefreshDatabase;
  /**
   * A basic test example.
   *
   * @return void
   */
- public function testSongDatabase()
+ public function testSongDatabase1()
  {
   $this->assertTrue(
    Schema::hasColumns('books', [
@@ -23,5 +24,24 @@ class DatabaseSongTest extends TestCase
    ]),
    // 1
   );
+ }
+
+ public function testSongDatabase2()
+ {
+  $song = new Song();
+  $song->title = 'test1';
+  $song->detail = 'test2';
+  $saveSong = $song->save();
+
+  $this->assertTrue($saveSong);
+ }
+
+ public function testSongDatabase3()
+ {
+  $song = [
+   'title' => 'test1',
+  ];
+
+  $this->assertDatabaseHas('songs', $song);
  }
 }
