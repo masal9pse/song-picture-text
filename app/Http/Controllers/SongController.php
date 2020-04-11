971372a2fdf07becc2fdf3cba58b4d896b77b469
masato <returnymgstokh@icloud.com>
2020-04-11 22:54:30 +0900

    トップページの検索機能実装

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -13,17 +13,29 @@ class SongController extends Controller
   *
   * @return \Illuminate\Http\Response
   */
- public function index()
+ public function index(Request $request)
  {
-  // $songs = Song::all();
-  $songs = DB::table('songs')
-   ->select('id', 'title', 'detail', 'created_at')
-   ->orderBy('created_at', 'asc')
-   ->paginate(10);
-  // dd($songs);
-  return view('songs.index', [
-   'songs' => $songs
-  ]);
+  $search = $request->input('search');
+  $query = DB::table('songs');
+
+  // もしキーワードがあったら
+  if ($search !== null) {
+   // 半角スペースを半角に
+   $search_split = mb_convert_kana($search, 's');
+
+   // 空白で区切る
+   $search_split2 = preg_split('/[\s]+/', $search_split, -1, PREG_SPLIT_NO_EMPTY);
+
+   foreach ($search_split2 as $value) {
+    $query->where('title', 'like', '%' . $value . '%');
+   }
+  }
+
+  $query->select('id', 'title', 'detail', 'created_at');
+  $query->orderBy('created_at', 'asc');
+  $songs = $query->paginate(10);
+
+  return view('songs.index', compact('songs'));
  }
 
  /**
