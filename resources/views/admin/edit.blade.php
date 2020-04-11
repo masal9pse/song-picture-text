7e5bf8dba7258635a26fb457ff782a350442fa06
masato <returnymgstokh@icloud.com>
2020-04-11 17:42:10 +0900

    更新処理のスタイルのみ実装

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,1 +1,33 @@
-aaaa
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+ <div class="row justify-content-center">
+  <div class="col-md-8">
+   <div class="card">
+    <div class="card-header">Dashboard</div>
+
+    <div class="card-body">
+     @if (session('status'))
+     <div class="alert alert-success" role="alert">
+      {{ session('status') }}
+     </div>
+     @endif
+
+     editです
+     <form action="" method="post">
+      {{ csrf_field() }}
+      タイトル
+      <input type="text" name="title" value="{{ $song->title }}">
+      <br>
+      歌詞
+      <textarea name="detail">{{ $song->detail }}</textarea>
+      <input type="submit" value="更新する" class="btn btn-info">
+    </div>
+    </form>
+   </div>
+  </div>
+ </div>
+</div>
+</div>
+@endsection
