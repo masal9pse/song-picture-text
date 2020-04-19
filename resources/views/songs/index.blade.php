d22262db349b432d3543f6ef9f8f0a36ea0c992f
masato <returnymgstokh@icloud.com>
2020-04-19 20:21:09 +0900

    containerでスタイル安定

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 @section('content')
-{{-- <form method="GET" action="{{ route('songs.index') }}" class="form-inline my-2 my-lg-0"> --}}
-{{ Form::open(['method' => 'get'],['route' => 'admin.create'],['class' => "form-inline my-2 my-lg-0"]) }}
-<input class="form-control mr-sm-2" name="search" type="search" placeholder="検索" aria-label="Search">
-<button class="btn btn-success my-2 my-sm-0" type="submit">検索する</button>
-{{Form::close()}}
+<div class="container">
+ {{-- <form method="GET" action="{{ route('songs.index') }}" class="form-inline my-2 my-lg-0"> --}}
+ {{ Form::open(['method' => 'get'],['route' => 'admin.create'],['class' => "form-inline my-2 my-lg-0"]) }}
+ <input class="form-control mr-sm-2" name="search" type="search" placeholder="検索" aria-label="Search">
+ <button class="btn btn-success my-2 my-sm-0" type="submit">検索する</button>
+ {{Form::close()}}
 
-@foreach ($songs as $song)
-<p>
- <a href="{{ route('songs.show',['id' => $song->id]) }}">
-  {{ $song->title }}
- </a>
-</p>
-@endforeach
-{{ $songs->links() }}
+ @foreach ($songs as $song)
+ <p>
+  <a href="{{ route('songs.show',['id' => $song->id]) }}">
+   {{ $song->title }}
+  </a>
+ </p>
+ @endforeach
+ {{ $songs->links() }}
+</div>
 @endsection
